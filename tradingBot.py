7fee2dd41b3fe4fd005c3ca27d9f2f71ebbeac61
EwanDebove <ewan.debove@gmail.com>
2020-01-10 15:14:18 +0100

    getNames function

--- tradingBot.py
+++ tradingBot.py
@@ -5,3 +5,15 @@ Created on Fri Jan 10 14:25:43 2020
 @author: Ewan Debove
 """
 
+import requests
+
+r=requests.get('https://api.coinbase.com/v2/currencies')
+
+response = r.json()
+
+currencies=[]
+
+for i in range(len(response['data'])):
+    currencies.append(response['data'][i]['name'])
+    
+print(currencies)
